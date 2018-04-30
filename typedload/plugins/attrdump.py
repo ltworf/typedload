0c2c8ec317827510e452147b9d79febca555a0fe
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-04-30 11:41:04 +0200

    New plugin to dump using attr

---
+++ typedload/plugins/attrdump.py
@@ -0,0 +1,39 @@
+# typedload
+# Module to load data into data structures from the "attr" module
+
+# Copyright (C) 2018 Salvo "LtWorf" Tomaselli
+#
+# typedload is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
+
+
+from typing import Any, Dict
+
+
+def _condition(value: Any) -> bool:
+    return hasattr(value, '__attrs_attrs__')
+
+
+def _attrdump(d, value) -> Dict[str, Any]:
+    r = {}
+    for attr in value.__attrs_attrs__:
+        attrval = getattr(value, attr.name)
+        if not (d.hidedefault and attrval == attr.default):
+            r[attr.name] = d.dump(attrval)
+    return r
+
+
+def add2dumper(l) -> None:
+    l.handlers.append((_condition, _attrdump))
