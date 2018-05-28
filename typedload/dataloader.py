189d30ff3c08d8236cc09bd2f1ac668dce3cac1e
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-05-28 17:19:32 +0200

    Make mypy happy

--- typedload/dataloader.py
+++ typedload/dataloader.py
@@ -44,7 +44,7 @@ def _issubclass(t1, t2) -> bool:
 
 # This is a workaround for an incompatibility between 3.5.2 and previous, and 3.5.3 and later
 try:
-    issubclass(Union[int,str], Union)
+    issubclass(Union[int,str], Union)  # type: ignore
     HAS_UNIONSUBCLASS = True
 except:
     HAS_UNIONSUBCLASS = False
