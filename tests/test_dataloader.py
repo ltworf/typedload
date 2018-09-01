e2f6be548a5b86b15ade3f6fbba48fd6d28394c2
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2018-09-02 01:39:57 +0200

    More ForwardRef tests

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -204,6 +204,23 @@ class TestForwardRef(unittest.TestCase):
         loader = dataloader.Loader()
         assert loader.load(l, Node) == Node(value=12,next=Node())
 
+    def test_disable(self):
+        class A(NamedTuple):
+            i: 'int'
+        loader = dataloader.Loader(frefs=None)
+        with self.assertRaises(Exception):
+            loader.load(3, A)
+
+    def test_add_fref(self):
+        class A(NamedTuple):
+            i: 'alfio'
+        loader = dataloader.Loader()
+        with self.assertRaises(ValueError):
+            loader.load({'i': 3}, A)
+        loader.frefs['alfio'] = int
+        assert loader.load({'i': 3}, A) == A(3)
+
+
 
 class TestLoaderIndex(unittest.TestCase):
 
