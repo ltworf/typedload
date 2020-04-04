b4dc0f922f29073e1f8131d0a88c6aa9019ab1b8
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2020-04-04 13:35:52 +0200

    Replace example with one that works
    The previous example used the Yahoo weather API which was discontinued a while ago.
    
    Better to use an example that actually works.

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # typedload
-# Copyright (C) 2018 Salvo "LtWorf" Tomaselli
+# Copyright (C) 2020 Salvo "LtWorf" Tomaselli
 #
 # typedload is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,118 +18,151 @@
 #
 # author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
 
-#This is a basic example on how to use the typedload library.
+# This is a practical example on how to use the typedload library.
 
-#Json data is downloaded from the internet and then loaded into
-#Python data structures (dictionaries, lists, strings, and so on).
+# Json data is downloaded from the internet and then loaded into
+# Python data structures (dictionaries, lists, strings, and so on).
 
-#This example queries Yahoo weather and prints the forecast.
+#This example queries github API
 
+import argparse
+from datetime import datetime
+from enum import Enum
 import json
-import sys
-from typing import Any, Dict, List, NamedTuple, Optional
+from typing import *
 import urllib.request
 
 import typedload
 
 
-def get_url(city: Optional[str]) -> str:
-    """
-    Get the URL for the Yahoo weather API for a
-    given city
-    """
-    if not city:
-        city = 'Catania'
-    return "https://query.yahooapis.com/v1/public/yql?q=sele" \
-        "ct%20*%20from%20weather.forecast%20where%20w" \
-        "oeid%20in%20(select%20woeid%20from%20geo.pla" \
-        "ces(1)%20where%20text%3D%22" + city + "%22)%" \
-        "20and%20u%3D'c'&format=json&env=store%3A%2F%" \
-        "2Fdatatables.org%2Falltableswithkeys"
+class CommandLine(NamedTuple):
+    full: bool
+    project: Optional[str]
+    username: Optional[str]
+
+    def get_url(self) -> str:
+        if self.username is None and self.project is None:
+            username = 'ltworf'
+            project = 'typedload'
+        elif self.username and self.project:
+            username = self.username
+            project = self.project
+        else:
+            raise ValueError('Username and project need to be set together')
+        return 'https://api.github.com/repos/%s/%s/releases' % (username, project)
 
 
-def get_data(city: Optional[str]) -> Dict[str, Any]:
+class UserTypes(Enum):
+    #FIXME I could not find the documentation of what the other values can be
+    USER = 'User'
+
+
+class AssetState(Enum):
+    #FIXME I could not find the documentation of what the other values can be
+    UPLOADED = 'uploaded'
+
+
+class User(NamedTuple):
+    login: str
+    id: int
+    node_id: str
+    avatar_url: str
+    gravatar_id: str
+    url: str
+    html_url: str
+    followers_url: str
+    following_url: str
+    gists_url: str
+    starred_url: str
+    subscriptions_url: str
+    organizations_url: str
+    repos_url: str
+    received_events_url: str
+    type: UserTypes
+    site_admin: bool
+
+
+class Asset(NamedTuple):
+    url: str
+    id: int
+    node_id: str
+    name: str
+    uploader: User
+    content_type: str
+    state: AssetState
+    size: int
+    download_count: int
+    created_at: datetime
+    updated_at: datetime
+    browser_download_url: str
+
+
+class Release(NamedTuple):
+    url: str
+    assets_url: str
+    upload_url: str
+    html_url: str
+    id: int
+    node_id: str
+    tag_name: str
+    target_commitish: str
+    name: str
+    draft: bool
+    author: User
+    prerelease: bool
+    created_at: datetime
+    published_at: datetime
+    assets: List[Asset]
+
+
+def get_data(args: CommandLine) -> Any:
     """
-    Use the Yahoo weather API to get weather information
+    Use the github API to get releases information
     """
-    req = urllib.request.Request(get_url(city))
+    req = urllib.request.Request(args.get_url())
     with urllib.request.urlopen(req) as f:
-        response = f.read()
-    answer = response.decode('ascii')
-    data = json.loads(answer)
-    r = data['query']['results']['channel']  # Remove some useless nesting
-    return r
+        return json.load(f)
 
 
-class Units(NamedTuple):
-    distance: str
-    pressure: str
-    speed: str
-    temperature: str
+def print_report(data: List[Release], args: CommandLine):
+    for i in data:
+        if i.draft or i.prerelease:
+            continue
+        print('Release:', i.name, end=' ')
 
+        if args.full:
+            print('Created by:', i.author.login, 'on:', i.created_at)
+        else:
+            print()
 
-class Astronomy(NamedTuple):
-    sunrise: str
-    sunset: str
-
-
-class Atmosphere(NamedTuple):
-    humidity: int
-    pressure: float
-    rising: int
-    visibility: float
-
-class Forecast(NamedTuple):
-    date: str
-    day: str
-    high: int
-    low: int
-    text: str
-
-
-class Condition(NamedTuple):
-    date: str
-    temp: int
-    text: str
-
-
-class Item(NamedTuple):
-    forecast: List[Forecast]
-    condition: Condition
-    title: str
-
-
-class Wind(NamedTuple):
-    chill: int
-    direction: int
-    speed: float
-
-
-class Weather(NamedTuple):
-    item: Item
-    units: Units
-    astronomy: Astronomy
-    atmosphere: Atmosphere
-    wind: Wind
+        for asset in i.assets:
+            if asset.download_count or args.full:
+                print('\t%d\t%s' % (asset.download_count, asset.name))
 
 
 def main():
-    raw_data = get_data(sys.argv[1] if len(sys.argv) == 2 else None)
-    weather = typedload.load(raw_data, Weather)
-    print(weather.item.title)
-    print()
-    print('Sunrise %s\t Sunset %s' % (weather.astronomy.sunrise, weather.astronomy.sunset))
-    print()
-    print('%s %s%s' % (weather.item.condition.text, weather.item.condition.temp, weather.units.temperature))
-    print()
-    print('Wind: %d%s' % (weather.wind.speed, weather.units.speed))
-    print('Humidity: %s%%' % (weather.atmosphere.humidity, ))
-    print('Pressure: %s%s' % (weather.atmosphere.pressure, weather.units.pressure))
-    print('Visibility: %s%s' % (weather.atmosphere.humidity, weather.units.distance))
-    print()
-    print('Forecast')
-    for i in weather.item.forecast:
-        print('%s\tMin: %2s%s\tMax: %2s%s\t%s' % (i.day, i.low, weather.units.temperature, i.high, weather.units.temperature, i.text))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', help='The username to query')
+    parser.add_argument('-p', '--project', help='The project to query')
+    parser.add_argument('-f', '--full', help='Print the full report', action='store_true')
+
+    # We load the args into a NamedTuple, so it is no longer an obscure dynamic object but it is typed
+    args = typedload.load(parser.parse_args(), CommandLine)
+    data = get_data(args)
+
+
+    # Github returns dates like this "2016-08-23T18:26:00Z", which are not supported by typedload
+    # So we make a custom handler for them.
+    loader = typedload.dataloader.Loader()
+    loader.handlers.insert(0, (# Make the new handler first so it overrides the current handler for datetime
+        lambda t: t == datetime,
+        lambda l, v, t: datetime.fromisoformat(v[:-1])
+    ))
+
+
+    # We know what the API returns so we can load the json into typed data
+    typed_data = loader.load(data, List[Release])
+    print_report(typed_data, args)
 
 
 if __name__ == '__main__':
