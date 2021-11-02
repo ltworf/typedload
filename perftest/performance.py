8a27cc52376eea67bf6778481c2287b4b14a41b8
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2021-11-02 14:40:13 +0100

    Run tests from a temp dir

--- perftest/performance.py
+++ perftest/performance.py
@@ -19,12 +19,19 @@
 
 
 from subprocess import check_output, DEVNULL
+from tempfile import mkdtemp
+from shutil import copy, rmtree
 
 
 def main():
     tests = [
         'perf_intlist',
     ]
+
+    tempdir = mkdtemp()
+    for i in tests:
+        copy(f'perftest/{i}.py', tempdir)
+
 
     tags = check_output(['git', 'tag', '--list'], encoding='ascii').strip().split('\n')
 
@@ -35,17 +42,19 @@ def main():
             counter = 0
 
             print('\tRunning test with pydantic')
-            pydantic_time = float(check_output(['python3', f'perftest/{i}.py', '--pydantic']))
+            pydantic_time = float(check_output(['python3', f'{tempdir}/{i}.py', '--pydantic']))
             f.write(f'{counter} "pydantic" {pydantic_time}\n')
             for branch in tags[len(tags)-4:] + ['master']:
                 counter += 1
                 print(f'\tRunning test with {branch}')
                 check_output(['git', 'checkout', branch], stderr=DEVNULL)
-                typedload_time = float(check_output(['python3', f'perftest/{i}.py', '--typedload']))
+                typedload_time = float(check_output(['python3', f'{tempdir}/{i}.py', '--typedload']))
                 f.write(f'{counter} "typedload {branch}" {typedload_time}\n')
 
             counter += 1
             f.write(f'{counter} "0" {0}\n')
+    rmtree(tempdir)
+
 
 if __name__ == '__main__':
     main()
