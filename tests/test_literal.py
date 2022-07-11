e26908e3ea23279817da43653a7c10c95f46ed02
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-07-11 03:50:39 +0200

    Some initial tests for the function

--- tests/test_literal.py
+++ tests/test_literal.py
@@ -17,7 +17,7 @@
 # author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
 
 
-from typing import Literal
+from typing import Literal, NamedTuple
 import unittest
 
 from typedload import dataloader, load, dump, typechecks
@@ -41,3 +41,25 @@ class TestLiteralLoad(unittest.TestCase):
         l = Literal[1, 2, 'a']
         with self.assertRaises(ValueError):
             load(3, l)
+
+    def test_discriminatorliterals_wrong(self):
+        assert typechecks.discriminatorliterals(int) == {}
+
+    def test_discriminatorliterals_namedtuple(self):
+        class A(NamedTuple):
+            t: Literal['a', 'b']
+            i: int
+            q: str
+
+        class B(NamedTuple):
+            t: Literal[33]
+            q: Literal[12]
+            i: int
+
+        class C(NamedTuple):
+            t: Literal['a']
+            i: int
+
+        assert typechecks.discriminatorliterals(A) == {'t': {'a', 'b'}}
+        assert typechecks.discriminatorliterals(B) == {'t': {33,}, 'q': {12,}}
+        assert typechecks.discriminatorliterals(C) == {'t': {'a', }}
