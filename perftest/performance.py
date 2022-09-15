458a4399703875a40b39d5e215288e221da942f5
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-09-15 22:12:29 +0200

    Improve style

--- perftest/performance.py
+++ perftest/performance.py
@@ -124,7 +124,7 @@ def main():
                 counter += 1
         with Popen(['gnuplot'], stdin=PIPE, encoding='ascii') as p:
             f = p.stdin
-            print('set style fill transparent solid 0.2 noborder', file=f)
+            print('set style fill solid 0.2 noborder', file=f)
             print('set ylabel "seconds"', file=f)
             print('set xlabel "package"', file=f)
             print(f'set title "typedload performance test {sys.version}"', file=f)
