6ea230c319ea1e31f8415e992546b2318acf1b14
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2022-12-01 17:42:14 +0100

    Add keywords

--- gensetup.py
+++ gensetup.py
@@ -64,7 +64,23 @@ def load_version():
         return f.readline().strip()
 
 AUTHOR = 'Salvo \'LtWorf\' Tomaselli'
-
+AUTHOR_EMAIL = 'tiposchi@tiscali.it'
+URL = 'https://ltworf.github.io/typedload/'
+BUGTRACKER = 'https://github.com/ltworf/typedload/issues'
+DESCRIPTION = 'Load and dump data from json-like format into typed data structures'
+KEYWORDS='typing types mypy json'
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+]
 
 if len(argv) != 2:
     exit('Wrong command line')
@@ -78,34 +94,39 @@ from setuptools import setup
 setup(
     name='typedload',
     version={load_version()!r},
-    description='Load and dump data from json-like format into typed data structures',
+    description='{DESCRIPTION}',
     long_description={''.join(load_long_description())!r},
-    url='https://ltworf.github.io/typedload/',
+    url='{URL}',
     author={AUTHOR!r},
-    author_email='tiposchi@tiscali.it',
+    author_email='{AUTHOR_EMAIL}',
     license='GPLv3',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-    ],
-    keywords='typing types mypy json',
+    classifiers={CLASSIFIERS!r},
+    keywords='{KEYWORDS}',
     packages=['typedload'],
     package_data={{"typedload": ["py.typed", "__init__.pyi"]}},
 )''', file=f
     )
 
-if argv[1] == '--pyproject.toml':
+elif argv[1] == '--pyproject.toml':
     with open('pyproject.toml', 'wt') as f:
         print(
-'''
+f'''
+[project]
+name = "typedload"
+version = "{load_version()!r}"
+authors = [
+  {{ name="{AUTHOR}", email="{AUTHOR_EMAIL}" }},
+]
+description = "{DESCRIPTION}"
+readme = "README.md"
+requires-python = ">=3.5"
+classifiers = {CLASSIFIERS!r}
+keywords = "{KEYWORDS}"
+
+[project.urls]
+"Homepage" = "{URL}"
+"Bug Tracker" = "{BUGTRACKER}"
+
 [build-system]
 requires = ["setuptools", "wheel"]
 ''', file=f
