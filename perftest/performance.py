01a7ce1c6d2d8d8ad4a10f9253436688ec523fdf
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2023-01-11 07:26:06 +0100

    pypy uses multiple lines for version

--- perftest/performance.py
+++ perftest/performance.py
@@ -116,13 +116,14 @@ def main():
                 counter += 1
         with Popen(['gnuplot'], stdin=PIPE, encoding='ascii') as p:
             f = p.stdin
+            version = sys.version.replace('\n', ' ')
             print('set style fill solid 0.2 noborder', file=f)
             print('set ylabel "seconds"', file=f)
             print('set xlabel "package"', file=f)
             print('set boxwidth 0.8', file=f)
             print('set xlabel "x-units" font "Times-Roman,12"', file=f)
             print('set style fill solid 1.0', file=f)
-            print(f'set title "typedload performance test {sys.version}"', file=f)
+            print(f'set title "typedload performance test {version}"', file=f)
             print(f'set yrange [-0.5:{test_maxtime}]', file=f)
             print('set term svg', file=f)
             print(f'set output "{outdir}/{PYVER}_{t.replace(" ", "_")}.svg"', file=f)
